c358497ec5ce62db3bef2da97319e99206896403
Tina Tram <tinatram07@gmail.com>
2022-10-28 11:33:56 -0500

    added 6 album objects to collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,4 +10,9 @@ function addToCollection(title, artist, yearPublished) {
     return record;
 }
 console.log(addToCollection('Midnights', 'Taylor Swift', 2022));
+console.log(addToCollection('Ambitions', 'ONE OK ROCK', 2017));
+console.log(addToCollection('Be the Cowboy', 'Mitski', 2018));
+console.log(addToCollection('American Beauty/American Psycho', 'Fall Out Boy', 2015));
+console.log(addToCollection('Epik High is Here Pt.2', 'Epik High', 2022));
+console.log(addToCollection('Justice', 'Justin Bieber', 2021));
 console.log(collection);
