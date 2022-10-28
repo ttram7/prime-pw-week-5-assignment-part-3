9da26a534dc2ea54d013db23b98872f56e10110f
Tina Tram <tinatram07@gmail.com>
2022-10-28 11:27:27 -0500

    created object and added to collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,5 +1,13 @@
 console.log('***** Music Collection *****')
 let collection = [];
 function addToCollection(title, artist, yearPublished) {
-    
+    let record = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(record);
+    return record;
 }
+console.log(addToCollection('Midnights', 'Taylor Swift', 2022));
+console.log(collection);
