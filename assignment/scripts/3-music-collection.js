5b13cb87e7161616fd4f11774c5004f8d9e00214
Tina Tram <tinatram07@gmail.com>
2022-10-28 11:53:41 -0500

    created findByArtist function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,10 +9,32 @@ function addToCollection(title, artist, yearPublished) {
     collection.push(record);
     return record;
 }
+
+function showCollection(currentCollection) {
+    console.log('Number of albums in current collection:', currentCollection.length);
+    for (album of currentCollection) {
+        console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+    }
+}
+
+function findByArtist(artist) {
+    let results = [];
+    for (album of collection) {
+        if (album.artist === artist) {
+            results.push(album);
+        }
+    }
+    return results;
+}
+
 console.log(addToCollection('Midnights', 'Taylor Swift', 2022));
 console.log(addToCollection('Ambitions', 'ONE OK ROCK', 2017));
 console.log(addToCollection('Be the Cowboy', 'Mitski', 2018));
 console.log(addToCollection('American Beauty/American Psycho', 'Fall Out Boy', 2015));
-console.log(addToCollection('Epik High is Here Pt.2', 'Epik High', 2022));
+console.log(addToCollection('Infinity On High', 'Fall Out Boy', 2007));
 console.log(addToCollection('Justice', 'Justin Bieber', 2021));
 console.log(collection);
+
+showCollection(collection);
+console.log('Matching artist(s):',findByArtist('Fall Out Boy'));
+console.log('Artist not found:',findByArtist('Nicki Minaj'));
